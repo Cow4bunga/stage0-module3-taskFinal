597e4fd74ce4d984f7bf1f997ff98bb05d12d64a
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-03 13:28:20 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int result=0;
+
+        while (number!=0){
+            int rem=number%10;
+            result=result*10+rem;
+            number/=10;
+        }
+        System.out.println(result);
     }
 }
